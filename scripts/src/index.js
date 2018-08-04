bdf4c7bc9c2095a9e8de87fe461cfc2463eeb9c5
Max Wienold <dr0wned@web.de>
2018-08-04 18:43:28 +0200

    .gitignore

--- scripts/src/index.js
+++ scripts/src/index.js
@@ -3,6 +3,7 @@ import questions from './lib/questions';
 import answers from './lib/answers';
 import buttons from './lib/buttons';
 import result from './lib/result';
+
 function startQuiz(args) {
   questions.init();
 }
